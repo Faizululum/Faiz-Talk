e2313fea2512febcbb2c61a2b2dd7b601f5b8e17
Faizululum <faizululum25@gmail.com>
2025-04-13 20:18:19 +0700

    feat: create interview card component and random function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -18,20 +20,26 @@ const page = () => {
             </Link>
           </Button>
         </div>
-        <Image src="/robot.png" alt="robot" width={400} height={400} />
+        <Image src="/robot.png" alt="robot" width={400} height={400} className="max-md:hidden" />
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          <p>You haven&apos;t started any interviews yet.</p>
+          {/* <p>You haven&apos;t started any interviews yet.</p> */}
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
         </div>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-          <p>There are no interviews available at the moment.</p>
+          {/* <p>There are no interviews available at the moment.</p> */}
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
         </div>
       </section>
     </>
