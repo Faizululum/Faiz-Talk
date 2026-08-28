345b16b9bef12c42cc69f935cb58ae79955c3a5c
Faizululum <faizululum25@gmail.com>
2026-08-28 22:25:16 +0700

    feat(interview): implement interview fetching and display with user context

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import InterviewCard from '@/components/InterviewCard'
-import { Button } from '@/components/ui/button'
-import { getCurrentUser, getInterviewByUserId, getLatestInterviews } from '@/lib/actions/auth.action'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import InterviewCard from '@/components/InterviewCard';
+import { Button } from '@/components/ui/button';
+import { getCurrentUser } from '@/lib/actions/auth.action';
+import { getInterviewByUserId, getLatestInterviews } from '@/lib/actions/general.action';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const page = async () => {
   const user = await getCurrentUser();
